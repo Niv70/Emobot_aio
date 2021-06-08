905f3ccae9143f26d932f50acbaad60626141b76
Иван Носов <ivannsov70@gmail.com>
2021-06-08 19:06:06 +0700

    Создание БД

--- handlers/users/dbcreate.py
+++ handlers/users/dbcreate.py
@@ -7,8 +7,8 @@ from sqlalchemy import and_
 from utils.db_api.models import Emo_users, Emotions, Tasks
 from utils.db_api.database import db, create_db
 
+
 @dp.message_handler(Command("dbcreate"), state='*')
 async def bot_restart(message: types.Message):
     await message.answer("DBCREATE")
     await create_db()
-

bc1cb2c7d6ec27e89e1d2e21ef86c5b5e86783ec
Иван Носов <ivannsov70@gmail.com>
2021-06-09 20:05:22 +0700

    Создание БД

--- utils/db_api/database.py
+++ utils/db_api/database.py
@@ -4,11 +4,29 @@ from data.config import POSTGRES_URI
 
 db = Gino()
 
-async def create_db():
+
+async def open_db():
     # Устанавливаем связь с базой данных
     await db.set_bind(POSTGRES_URI)
     db.gino: GinoSchemaVisitor
-    await db.gino.drop_all()
+
+async def create_db():
+    # Создаем таблицы
+    await db.set_bind(POSTGRES_URI)
+    db.gino: GinoSchemaVisitor
+    #await db.gino.drop_all()
     await db.gino.create_all()
+    await db.pop_bind().close()
+
+async def drop_db():
+    await db.set_bind(POSTGRES_URI)
+    db.gino: GinoSchemaVisitor
+    await db.gino.drop_all()
+    await db.pop_bind().close()
+
+async def close_db():
+    await db.pop_bind().close()
+
+
 
 

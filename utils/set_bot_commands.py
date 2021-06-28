3bd612fae36c901441d5095f126e2cbc15f99a5a
centavrr <vl_shestakov@mail.ru>
2021-06-29 00:05:28 +0600

    Убрал /reset из подсказки.

--- utils/set_bot_commands.py
+++ utils/set_bot_commands.py
@@ -8,7 +8,7 @@ async def set_default_commands(dp):
             types.BotCommand("help", "Вывести справку"),
             types.BotCommand("stop", "Остановить бота"),
             types.BotCommand("stat", "Статистика по зафиксированным эмоциям"),
-            types.BotCommand("reset", "Перезапуск бота"),
+            # types.BotCommand("reset", "Перезапуск бота"),
             # types.BotCommand("dbdrop", "Зачистить базу"),
             # types.BotCommand("dbcreate", "Создать таблицы"),
         ]

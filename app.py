80ee4c36454140672bb388ac4e692b6625128228
Иван Носов <ivannsov70@gmail.com>
2021-11-18 16:47:52 +0600

    mmmenu+ 3

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
-import asyncio
 from aiogram import executor
 
-from loader import dp, storage
-import middlewares, filters, handlers
+from loader import dp
+from utils.db_api.database import open_db, close_db
 from utils.notify_admins import on_startup_notify
 from utils.set_bot_commands import set_default_commands
-from utils.db_api.database import db, open_db, close_db
 
 
 async def on_startup(dispatcher):
